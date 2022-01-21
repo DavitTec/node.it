8873eab8f99170a86d96273864a34a4cfa915e4d
DavitTech <davittech@davit.ie>
2022-01-21 18:12:41 +0100

    @DavitTec

--- server.js
+++ server.js
@@ -5,10 +5,15 @@ http.createServer((request, response) => {
 
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.write('<h1>Hello, World!</h1>');
+  response.write('A simple HTML service');
   response.end();
 
 }).listen(port);
 
-console.log(`server simple server on port ${port}`);
+console.log(`simple html server on <a href="http://localhost:${port}>Node.it</a>
+
+to Close
+  crtl C or close terminal
+ `);
 
 // https://zetcode.com/javascript/http/
