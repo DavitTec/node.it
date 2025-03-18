26547fa870db7be220785d1f6d696485655110b6
David Mullins <me@davit.ie>
2025-03-18 21:02:52 +0100

    add: app.cjs

---
+++ components/main.mjs
@@ -0,0 +1,10 @@
+export default function main() {
+  return `
+      <main>
+          <section>
+              <h2>Main Content</h2>
+              <p>This is the main content of the page.</p>
+          </section>
+      </main>
+  `;
+}
